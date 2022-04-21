fbab15db4cf2ffb4352048314b88a8351365c881
himanshuprasad789 <71255657+himanshuprasad789@users.noreply.github.com>
2022-04-21 23:26:09 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      animation:{
+        'spin-slow':'spin 10s linear infinite'
+      }
+    },
+  },
+  plugins: [],
+}
